1300d8912fa461ae462c2f53ba136093358321af
serafim gray <serafim619@gmail.com>
2021-07-05 09:38:23 +0300

    covered with tests function _getLocale from defaultParameters

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,35 @@ module.exports = {
     browser: true,
     es2020: true
   },
+
   extends: [
     'plugin:vue/essential',
     'standard'
   ],
+
   parserOptions: {
     ecmaVersion: 11,
     sourceType: 'module'
   },
+
   plugins: [
     'vue'
   ],
+
   rules: {
   },
-  ignorePatterns: ['**/src/data/products*.js', '**/src/data/foo*.js']
+
+  ignorePatterns: ['**/src/data/products*.js', '**/src/data/foo*.js'],
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
 module.exports = {
+  plugins: ['babel-plugin-rewire'],
   presets: ['@vue/cli-plugin-babel/preset']
 }
